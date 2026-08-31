eff0fcbc2bbc422ba1ed1d56217668b88843abc7
Cristina M <mitrea@umich.edu>
2026-08-31 16:08:06 -0400

    Added scripts for package check removed notebook.

---
+++ check_packages.py
@@ -0,0 +1,44 @@
+import numpy as np
+print("numpy", np.__version__)
+
+import pandas as pd
+print("pandas", pd.__version__)
+
+import matplotlib as plt
+print("matplotlib", plt.__version__)
+
+import seaborn as sn
+print("seaborn", sn.__version__)
+
+import plotnine as plt9
+print("plotnine", plt9.__version__)
+
+import sqlite3 as sql
+print("sqlite3", sql.sqlite_version)
+
+import sqlalchemy as sqlal
+print("sqlalchemy", sqlal.__version__)
+
+import unittest as ut
+print("unittest", "ok")
+
+import json as js
+print("json", "ok")
+
+import xml as xm
+print("xml", "ok")
+
+import scipy
+print("scipy", scipy.__version__)
+
+import sklearn
+print("scikit-learn", sklearn.__version__)
+
+import tensorflow as tf
+print("tensorflow", tf.__version__)
+
+import keras
+print("keras", keras.__version__)
+
+import torch
+print("torch", torch.__version__)
---
+++ install_extra_packages.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+pip install plotnine sqlalchemy scipy scikit-learn tensorflow keras torch
